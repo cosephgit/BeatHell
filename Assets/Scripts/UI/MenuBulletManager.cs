8515063283af688c516a5965f2f5a4711990133b
Seph <josephlukenorris@gmail.com>
2025-11-12 14:46:36 +0000

    BeatHell - pre Unity update

---
+++ Assets/Scripts/UI/MenuBulletManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// used to cull excess bullets in the menu (send them back to library if they are out of bounds)
+
+public class MenuBulletManager : MonoBehaviour
+{
+}
